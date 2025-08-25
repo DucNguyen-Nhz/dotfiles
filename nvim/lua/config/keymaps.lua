b20c7a1f38308218349d66e415d5eb720e6b3828
Nahzilian <nahz3112@gmail.com>
2025-08-25 10:52:17 +0700

    Update LSP and keymap for tmux

--- nvim/lua/config/keymaps.lua
+++ nvim/lua/config/keymaps.lua
@@ -1,3 +1,6 @@
 -- Keymaps for moving code blocks in and visual modes
 vim.keymap.set("v", "J", ":m '>+1<CR>gv=gv", { noremap = true, silent = true })
 vim.keymap.set("v", "K", ":m '<-2<CR>gv=gv", { noremap = true, silent = true })
+
+-- auto format a file
+vim.keymap.set("n", "<leader>f>", "gg=G", { noremap = true, silent = true })
--- nvim/lua/plugins/copilot.lua
+++ nvim/lua/plugins/copilot.lua
@@ -1,5 +1,22 @@
 return {
-    "github/copilot.vim",
-    config = function()
-    end
+  "github/copilot.vim",
+  cmd = { "Copilot", "Copilot status", "Copilot file" },
+  event = "VeryLazy",
+  config = function()
+    -- Define a toggle function
+    vim.keymap.set("n", "<leader>cc", function()
+      local status = vim.fn["copilot#Enabled"]()
+      if status == 1 then
+        vim.cmd("Copilot disable")
+        print("Copilot Disabled")
+      else
+        vim.cmd("Copilot enable")
+        print("Copilot Enabled")
+      end
+    end, { desc = "Toggle Copilot" })
+
+    -- Other useful bindings
+    vim.keymap.set("n", "<leader>cf", "<cmd>Copilot file<cr>", { desc = "Copilot File" })
+    vim.keymap.set("n", "<leader>cs", "<cmd>Copilot status<cr>", { desc = "Copilot Status" })
+  end,
 }
