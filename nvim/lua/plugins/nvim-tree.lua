14503d21ea3bad68fe23404b900e9f34250cea50
Doug Nguyen <ducng31cs@gmail.com>
2026-04-06 22:28:34 +0700

    Added more commands

--- nvim/lua/plugins/nvim-tree.lua
+++ nvim/lua/plugins/nvim-tree.lua
@@ -5,9 +5,24 @@ return {
   dependencies = {
     "nvim-tree/nvim-web-devicons",
   },
+	keys = {
+    { "<leader>t", "<cmd>NvimTreeToggle<CR>", desc = "Toggle NvimTree" },
+  },
   config = function()
+	
+	local on_attach = function (bufnr)
+		local api = require('nvim-tree.api')
+
+		local function opts(desc)
+			return { desc = "nvim-tree: " .. desc, buffer = bufnr, noremap = true, silent = true, nowait = true }
+		end
+
+		api.map.on_attach.default(bufnr)
+
+	end
+
     require("nvim-tree").setup {
-		
+		on_attach = on_attach
 	}
   end,
 }
